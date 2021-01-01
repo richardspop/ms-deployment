bd24cdf9d4b7203e4be943d1e0f23e428fda102a
Pop Richards <richardspop@gmail.com>
2021-01-01 15:04:50 +0000

    CICD auto change image tag

---
+++ cicd-update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [[ $1 == *"monitor-sender"* ]]; then
+	sed -Ei "s|MONITOR_SENDER_IMAGE=.*|MONITOR_SENDER_IMAGE=$1|g" deploy.sh
+elif [[ $1 == *"receiver-logger"* ]]; then
+	sed -Ei "s|RECEIVER_LOGGER_IMAGE=.*|RECEIVER_LOGGER_IMAGE=$1|g" deploy.sh
+else
+	echo "Error: Invalid Image tag"
+	exit -1
+fi
+
+git add .
+git commit -m ""
+git push origin
