549464bc0577b04e6a13d116e82a7dcc7b8ce59a
Pop Richards <richardspop@gmail.com>
2021-01-01 14:45:03 +0000

    Initial kustomization script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Configuration for deployment
+
+MONITOR_SENDER_IMAGE=10.157.1.216:5000/monitor-sender:v1-5-g3d1b170
+RECEIVER_LOGGER_IMAGE=10.157.1.216:5000/receiver-logger:v1-2-g0d265ac
+RECEIVER_LOGGER_FILE_PATH=/tmp/log_path.txt
+OVERLAY_PATH=`pwd`/ms-kustomize/overlay
+PATCH_FILE=${OVERLAY_PATH}/DaemonSet.yaml
+
+cp ${PATCH_FILE} ${PATCH_FILE}.bkp
+
+patch_replace () {
+	sed -Ei "s|$1|$2|g" "$PATCH_FILE"
+}
+
+patch_replace MONITOR_SENDER_IMAGE $MONITOR_SENDER_IMAGE
+
+patch_replace RECEIVER_LOGGER_IMAGE $RECEIVER_LOGGER_IMAGE
+
+patch_replace RECEIVER_LOGGER_FILE_PATH $RECEIVER_LOGGER_FILE_PATH
+
+kustomize build $OVERLAY_PATH | kubectl apply -f -
+
+mv ${PATCH_FILE}.bkp ${PATCH_FILE}
